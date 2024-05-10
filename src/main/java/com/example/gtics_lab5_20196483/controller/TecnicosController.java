178f779e9a3dc6751a8df98f1359d6c04a1ab4f7
Allison <a20196483@pucp.edu.pe>
2024-05-09 20:36:43 -0500

    formulario tecnicos

--- src/main/java/com/example/gtics_lab5_20196483/controller/TecnicosController.java
+++ src/main/java/com/example/gtics_lab5_20196483/controller/TecnicosController.java
@@ -4,10 +4,11 @@ import com.example.gtics_lab5_20196483.entity.Technician;
 import com.example.gtics_lab5_20196483.repository.TechnicianRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/tecnicos")
@@ -18,12 +19,41 @@ public class TecnicosController {
         this.technicianRepository = technicianRepository;
     }
 
-
     @GetMapping(value={"", "/"})
     public String listarTecnicos(Model model){
         List<Technician> listaTecnicos = technicianRepository.findAll();
         model.addAttribute("listaTecnicos",listaTecnicos);
         return "Tecnicos/listTecnicos";
     }
+    @GetMapping(value="/nuevo")
+    public String crearTecnico(@ModelAttribute("tecnico") Technician technician, Model model){
+        return "Tecnicos/formTecnicos";
+    }
 
+    @GetMapping("/editar")
+    public String editarTecnico(@ModelAttribute("tecnico") Technician technician,
+                                Model model,@RequestParam("id") int id) {
+
+        Optional<Technician> optTechnician = technicianRepository.findById(id);
+
+        if (optTechnician.isPresent()) {
+            technician = optTechnician.get();
+            model.addAttribute("tecnico", technician);
+            return "Tecnicos/formTecnicos";
+        } else {
+            return "redirect:/Tecnicos/listTecnicos";
+        }
+    }
+
+    @PostMapping("/guardar")
+    public String guardarTecnico(@ModelAttribute("tecnico") Technician technician,
+                                 RedirectAttributes attr) {
+        if(technician.getTechnicianID() == 0){
+            attr.addFlashAttribute("msg", "Tecnico creado exitósamente");
+        }else{
+            attr.addFlashAttribute("msg", "Tecnico actualizado exitosamente");
+        }
+        technicianRepository.save(technician);
+        return "redirect:/Tecnicos/listTecnicos";
+    }
 }
