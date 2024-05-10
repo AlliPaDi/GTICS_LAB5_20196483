3c97c2c6b5756d305e0d469a9ff538acd375cdb9
Allison <a20196483@pucp.edu.pe>
2024-05-09 19:17:30 -0500

    first commit

---
+++ src/main/java/com/example/gtics_lab5_20196483/GticsLab520196483Application.java
@@ -0,0 +1,13 @@
+package com.example.gtics_lab5_20196483;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GticsLab520196483Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GticsLab520196483Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gtics_lab5_20196483/GticsLab520196483ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gtics_lab5_20196483;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GticsLab520196483ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
